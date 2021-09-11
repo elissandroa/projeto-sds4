3767469142e43ace094837d5b47ec08cfb0571e0
Elissandro <elissandroa@hotmail.com>
2021-09-11 10:14:43 -0300

    First request

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -15,61 +15,70 @@ const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                <tr>
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr> <tr>
+                    </tr>
+                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr> <tr>
+                    </tr> 
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr> <tr>
+                    </tr> 
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr> <tr>
+                    </tr> 
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr> <tr>
+                    </tr> 
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr> <tr>
+                    </tr> 
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr> <tr>
+                    </tr> 
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr> <tr>
+                    </tr> 
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
-                    </tr> <tr>
+                    </tr> 
+                    <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
